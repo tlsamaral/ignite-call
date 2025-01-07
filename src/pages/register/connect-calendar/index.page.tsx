fc09febb9d76043911371dd137db8f4ead3503a1
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-06 22:13:35 -0300

    connect to calendar page

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,33 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ConnectBox, ConnectItem } from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+
+        <Button type="submit">
+          Proximo passo <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}

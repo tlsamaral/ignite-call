c94bc67db4742dfa515fe8591ea06fdea5d5308b
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-10 11:55:14 -0300

    update on next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  images: {
+    domains: ['https://lh3.googleusercontent.com/'],
+  },
 }
 
 export default nextConfig

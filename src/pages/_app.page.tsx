f2da96981d4a9ae2aca009b200b0117d0d2779ba
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2025-01-16 10:49:43 -0300

    default seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,6 +5,7 @@ import { globalStyles } from '@/styles/globals'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -15,6 +16,13 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            site_name: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
